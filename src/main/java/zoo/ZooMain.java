526e6b4ddf5bf678c5d60b955a25451f1433aa43
rcbaron <r.baron@live.de>
2025-06-30 13:59:07 +0200

    [Generic] add Habitat class + Test in Main

--- src/main/java/zoo/ZooMain.java
+++ src/main/java/zoo/ZooMain.java
@@ -1,7 +1,29 @@
 package zoo;
 
+import zoo.mammals.cats.Cat;
+import zoo.mammals.cats.Lion;
+import zoo.mammals.cats.Tiger;
+import zoo.mammals.primates.Chimpanzee;
+import zoo.mammals.primates.Orangutan;
+import zoo.mammals.primates.Primate;
+
 public class ZooMain {
     public static void main(String[] args) {
-        
+
+        // Habitat for Cat's
+        Habitat<Cat> catHabitat = new Habitat<>("CatHabitat");
+        catHabitat.add(new Lion());
+        catHabitat.add(new Tiger());
+        catHabitat.add(new Lion());
+        catHabitat.listAnimals();
+
+        //Habitat for Primates
+        Habitat<Primate> primateHabitat = new Habitat<>("PrimateHabitat");
+        primateHabitat.add(new Chimpanzee());
+        primateHabitat.add(new Orangutan());
+        primateHabitat.add(new Chimpanzee());
+        primateHabitat.listAnimals();
+
+
     }
 }
